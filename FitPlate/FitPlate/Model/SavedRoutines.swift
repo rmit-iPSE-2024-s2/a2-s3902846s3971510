8964cd57b478a0c49d9cdcea00fe3cb81f47a2d0
margaretxiiao <s3902846@student.rmit.edu.au>
2024-10-12 08:36:58 +1100

    added SavedRoutine Model, swiftData for saved routines from explore page and removed trending from home and

---
+++ FitPlate/FitPlate/Model/SavedRoutines.swift
@@ -0,0 +1,17 @@
+import Foundation
+import SwiftData
+
+@Model
+class SavedRoutine {  
+    var name: String
+    var imageName: String
+    var time: String
+    var workoutDescription: String
+
+    init(name: String, imageName: String, time: String, workoutDescription: String) {
+        self.name = name
+        self.imageName = imageName
+        self.time = time
+        self.workoutDescription = workoutDescription
+    }
+}
---
+++ FitPlate/FitPlate/Model/WorkoutViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutViewModel.swift
+//  FitPlate
+//
+//  Created by margaret on 12/10/2024.
+//
+
+import Foundation
